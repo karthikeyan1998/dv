a3aad6130a106050cbff895e9ba02e1d66f7475c
karthikeyan1998 <47741331+karthikeyan1998@users.noreply.github.com>
2019-03-25 16:16:14 +0530

    Create eveninterval.C

---
+++ eveninterval.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main(void)
+{
+    int i,a,b;
+    printf("\nEnter the number\n");
+    scanf("%d%d",&a,&b);
+    for(i=a+1;i<b;i++)
+    {
+        if(i%2==0)
+        {
+            printf("%d\n",i);
+            
+        }
+    }
+}
